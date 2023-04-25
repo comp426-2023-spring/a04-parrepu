117065e367c41ba5f568b6b96331d51cd0dd9bb4
Praneeth Arrepu <parrepu@live.unc.edu>
2023-04-25 14:57:32 -0400

    new

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Code goes here
 
 // Remember to import RPS and RPSLS module from a03 (stated in repo documentation)
-import { rps } from "./lib/rpsls.js"; // Right syntax?
-import { rpsls } from "./lib/rpsls.js";
+import {rps, rpsls} from "./lib/rpsls.js"; // Right syntax?
+// import { rpsls } from "./lib/rpsls.js";
 // Also import the dependencies you installed on Powershell
 import express from "express";
 import minimist from "minimist";
@@ -18,7 +18,9 @@ const app = express(); // Set name of variable to app as implied in documentatio
     // process.exit(0); 
 // }
 
-const portvar = arg2.portvar || 5000; // Another way (Debugging?)
+const port = arg2.port || 5000; // Another way (Debugging?) -> Port should default to 5000 if no argument is specified
+
+// Utilize app variable from earlier here
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
@@ -35,3 +37,14 @@ app.get('/app/rps/', (req, res) => {
     res.status(200).send(JSON.stringify(rps()));
 })
 
+// Operational requirement # 5 (just pass rpsls() into stringify instead)
+app.get('/app/rpsls/', (req, res) => {
+    // Use format from above
+    res.status(200).send(JSON.stringify(rpsls()));
+})
+
+// Operational requirement # 6 (Accept Request Bodies)
+app.get('/app/rps/play/', (req, res) => {
+    // What do I pass into rps? -> Update: pass 'req.query.shot' as an argument
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+})
