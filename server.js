089d346412339e81257f1e99206adedb27d1f28a
Praneeth Arrepu <parrepu@live.unc.edu>
2023-04-28 10:56:23 -0400

    quick check

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const arg2 = minimist(process.argv.slice(2))
 // Make another variable and set the value using the express import
 const app = express(); // Set name of variable to app as implied in documentation (pay attention to path -> ex. /app/rpsls/)
 
+// Alternate approach
 // if (portvar === null) { Port should default to 5000 if no argument is given | Much easier way of writing this (use '||' below)
     // portvar = 5000;
     // process.exit(0); 
@@ -22,22 +23,24 @@ const port = arg2.port || 5000; // Another way (Debugging? -> Works) -> Port sho
 
 // Utilize app variable from earlier here
 app.use(express.json());
+// Look at online documentation to get syntax for 'urlencoded'
 app.use(express.urlencoded({
     extended: true
 }))
 
 // Utilize "app.get()" to set the endpoints and ensure they return what's specified in the repo (GET method route)
-app.get('/app/', (req, res) => { // Reference online documentation to understand express routing and how to use 'app.get()'
+// Reference online documentation to understand express routing and how to use 'app.get()'
+app.get('/app/', (req, res) => { 
     res.status(200).send("200 OK"); // Use "200 OK" string as shown in instructions
 })
 
 // Operational requirement 
 app.get('/app/rps/', (req, res) => {
-    // Same format as above and utilize stringify (look a previous assignment to see how it works)
+    // Same format as above and utilize stringify (look a previous assignment to see how it works) and pass rps() in
     res.status(200).send(JSON.stringify(rps()));
 })
 
-// Operational requirement (just pass rpsls() into stringify instead)
+// Operational requirement (pass rpsls() instead of rps() into stringify)
 app.get('/app/rpsls/', (req, res) => {
     // Use format from above
     res.status(200).send(JSON.stringify(rpsls()));
